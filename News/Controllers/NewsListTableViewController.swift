4781b5dfda0611d492ff18c0fae48532e9a89f77
Gordon Choi <thefirate@gmail.com>
2022-01-15 00:33:53 +0900

    Attatch controller on table view controller

--- News/Controllers/NewsListTableViewController.swift
+++ News/Controllers/NewsListTableViewController.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+}
+
+extension NewsListTableViewController {
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
