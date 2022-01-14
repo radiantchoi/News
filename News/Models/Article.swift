311a5e4153d263354792a13bd3dcd66117afa0ac
Gordon Choi <thefirate@gmail.com>
2022-01-15 01:23:56 +0900

    Set model

---
+++ News/Models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  News
+//
+//  Created by Gordon Choi on 2022/01/15.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
