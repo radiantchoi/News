1e7d1cdafcaaa06bffd366477400c88c6ab94ad3
Gordon Choi <thefirate@gmail.com>
2022-01-15 16:29:21 +0900

    View model implement

---
+++ News/ViewModels/ArticleListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleListViewModel.swift
+//  News
+//
+//  Created by Gordon Choi on 2022/01/15.
+//
+
+import Foundation
+
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
